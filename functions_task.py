477db86c8dd878fffbfc0af1b3251002d578dd8a
NaserAldeen <30509@aum.edu.kw>
2019-08-01 20:26:36 +0300

    Much better algorithm for calculating the age

--- functions_task.py
+++ functions_task.py
@@ -15,10 +15,26 @@ def calculate_age(year,month,day):
      given_date = date(year = int(year), month = int(month), day = int(day))
      difference = date.today() - given_date
      difference = difference.days
-     print("You are {} years, {} months, and {} days".format(int((difference/365)),int((difference%365)/30),(difference%365)%12))
-    
+     years = int((difference/365))
+     year_int = int(year)
+     days_in_each_month = []
+     
+     while year_int <= 2019:
+         if (year_int%4 == 0) and ((not year_int%100) == 0 or (year_int%400 == 0)):
+             days_in_each_month.extend((31,29,31,30,31,30,31,31,30,31,30,31))
+         else:
+             days_in_each_month.extend((31,28,31,30,31,30,31,31,30,31,30,31))
+         year_int += 1
+             
+     months = 0
+     for day in days_in_each_month:
 
+         if difference >= day:
+             months += 1
+             difference -= day
+     print("You are {} years, {} months, and {} days".format(years,months%12,difference))
 
+ 
 year = input("Enter year of birth: ")
 month = input("Enter month of birth: ")
 day = input("Enter day of birth: ")
